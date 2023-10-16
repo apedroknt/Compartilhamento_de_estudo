96893fa8004420d977d77fb6b8191256999391f7
MateusCoelho023 <capic.2022118isinf0152@aluno.ifpi.edu.br>
2023-10-16 11:18:11 -0300

    Questionario adicionado

--- projeto/polls/views.py
+++ projeto/polls/views.py
@@ -3,3 +3,13 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Rafael um aumento por favor")
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s" % question_id)
+
+def results(request, question_id):
+    response= "You're looking at the results of question"
+    return HttpResponse(response % question_id)
+
+def vote (request, question_id):
+    return HttpResponse("You're voting on question %s" % question_id)
